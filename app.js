1c8ea3f2729edfafe8b8c124f0e4c849f5eec4fb
advocaat <adam.nicholls@my.jcu.edu.au>
2015-12-03 20:30:51 +1000

    still trying

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-//up the cowboys canny
+//fuck you cunt face
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
